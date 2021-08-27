1de2928d07fa82aaf9272a1d9b17e38b9b184b45
zxyyang <50910542+zxyyang@users.noreply.github.com>
2021-08-27 09:55:32 +0800

    swagger

--- src/main/java/com/toolbox/config/SwaggerConfig.java
+++ src/main/java/com/toolbox/config/SwaggerConfig.java
@@ -1,9 +1,10 @@
 package com.toolbox.config;
 
-import io.swagger.annotations.Api;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import io.swagger.annotations.Api;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
@@ -12,32 +13,23 @@ import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-
 /***
  * http://localhost:8081/swagger-ui.html#/
+ * http://localhost:8081/doc.html
  */
 @Configuration
 @EnableSwagger2
 @ConditionalOnProperty(prefix = "mconfig", name = "swagger-ui-open", havingValue = "true")
 public class SwaggerConfig {
 
-    @Bean
-    public Docket createRestApi() {
-        return new Docket(DocumentationType.SWAGGER_2)
-                .apiInfo(apiInfo())
-                .select()
-                .apis(RequestHandlerSelectors.withClassAnnotation(Api.class))
-                .paths(PathSelectors.any())
-                .build();
-    }
+	@Bean
+	public Docket createRestApi() {
+		return new Docket(DocumentationType.SWAGGER_2).apiInfo(apiInfo()).select().apis(RequestHandlerSelectors.withClassAnnotation(Api.class))
+				.paths(PathSelectors.any()).build();
+	}
 
-    public ApiInfo apiInfo() {
-        return new ApiInfoBuilder()
-                .title("ToolBox开放接口")
-                .description("Rest API接口")
-                .version("1.0")
-                .build();
-    }
+	public ApiInfo apiInfo() {
+		return new ApiInfoBuilder().title("ToolBox开放接口").description("Rest API接口").version("1.0").build();
+	}
 
 }
-
