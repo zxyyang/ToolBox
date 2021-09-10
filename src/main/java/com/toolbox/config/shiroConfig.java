8120ccb5c8e61fac8fc50607f8a6a0b3374a17f2
zxyyang <50910542+zxyyang@users.noreply.github.com>
2021-09-10 16:45:23 +0800

    shiro-redis

--- src/main/java/com/toolbox/config/shiroConfig.java
+++ src/main/java/com/toolbox/config/shiroConfig.java
@@ -1,58 +1,36 @@
 package com.toolbox.config;
 
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
 import org.apache.shiro.codec.Base64;
 import org.apache.shiro.mgt.SecurityManager;
+import org.apache.shiro.session.SessionListener;
+import org.apache.shiro.session.mgt.SessionManager;
+import org.apache.shiro.session.mgt.eis.SessionDAO;
+import org.apache.shiro.spring.LifecycleBeanPostProcessor;
 import org.apache.shiro.spring.security.interceptor.AuthorizationAttributeSourceAdvisor;
 import org.apache.shiro.spring.web.ShiroFilterFactoryBean;
 import org.apache.shiro.web.filter.authc.FormAuthenticationFilter;
 import org.apache.shiro.web.mgt.CookieRememberMeManager;
 import org.apache.shiro.web.mgt.DefaultWebSecurityManager;
 import org.apache.shiro.web.servlet.SimpleCookie;
+import org.apache.shiro.web.session.mgt.DefaultWebSessionManager;
+import org.crazycake.shiro.RedisCacheManager;
+import org.crazycake.shiro.RedisManager;
+import org.crazycake.shiro.RedisSessionDAO;
 import org.springframework.aop.framework.autoproxy.DefaultAdvisorAutoProxyCreator;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import com.toolbox.util.CustomRealm;
+import com.toolbox.util.ShiroSessionListener;
 
 @Configuration
 public class shiroConfig {
-
-	@Bean
-	@ConditionalOnMissingBean
-	public DefaultAdvisorAutoProxyCreator defaultAdvisorAutoProxyCreator() {
-		DefaultAdvisorAutoProxyCreator defaultAAP = new DefaultAdvisorAutoProxyCreator();
-		defaultAAP.setProxyTargetClass(true);
-		return defaultAAP;
-	}
-
-	// 将自己的验证方式加入容器
-	@Bean
-	public CustomRealm myShiroRealm() {
-		CustomRealm customRealm = new CustomRealm();
-		return customRealm;
-	}
-
-	// 权限管理，配置主要是Realm的管理认证
-	@Bean
-	public SecurityManager securityManager(CustomRealm myShiroRealm) {
-		DefaultWebSecurityManager securityManager = new DefaultWebSecurityManager();
-		// 设置自定义realm.
-		securityManager.setRealm(myShiroRealm);
-		// 配置记住我
-		securityManager.setRememberMeManager(rememberMeManager());
-
-		// 配置 redis缓存管理器 参考博客：
-		// securityManager.setCacheManager(getEhCacheManager());
-
-		// 配置自定义session管理，使用redis 参考博客：
-		// securityManager.setSessionManager(sessionManager());
-
-		return securityManager;
-	}
 
 	// Filter工厂，设置对应的过滤条件和跳转条件
 	@Bean
@@ -82,6 +60,49 @@ public class shiroConfig {
 		shiroFilterFactoryBean.setUnauthorizedUrl("/error");
 		shiroFilterFactoryBean.setFilterChainDefinitionMap(map);
 		return shiroFilterFactoryBean;
+	}
+
+	@Bean
+	@ConditionalOnMissingBean
+	public DefaultAdvisorAutoProxyCreator defaultAdvisorAutoProxyCreator() {
+		DefaultAdvisorAutoProxyCreator defaultAAP = new DefaultAdvisorAutoProxyCreator();
+		defaultAAP.setProxyTargetClass(true);
+		return defaultAAP;
+	}
+
+	// 将自己的验证方式加入容器
+	@Bean
+	public CustomRealm myShiroRealm() {
+		CustomRealm customRealm = new CustomRealm();
+		return customRealm;
+	}
+
+	// 权限管理，配置主要是Realm的管理认证
+	@Bean
+	public SecurityManager securityManager(CustomRealm myShiroRealm) {
+		DefaultWebSecurityManager securityManager = new DefaultWebSecurityManager();
+		// 设置自定义realm.
+		securityManager.setRealm(myShiroRealm);
+		// 配置记住我
+		securityManager.setRememberMeManager(rememberMeManager());
+
+		// 配置 redis缓存管理器 参考博客：
+		securityManager.setCacheManager(cacheManager());
+
+		// 配置自定义session管理，使用redis 参考博客：
+		securityManager.setSessionManager(sessionManager());
+
+		return securityManager;
+	}
+
+	/**
+	 * 配置Shiro生命周期处理器
+	 * 
+	 * @return
+	 */
+	@Bean(name = "lifecycleBeanPostProcessor")
+	public LifecycleBeanPostProcessor lifecycleBeanPostProcessor() {
+		return new LifecycleBeanPostProcessor();
 	}
 
 	/**
@@ -145,6 +166,113 @@ public class shiroConfig {
 		// 对应前端的checkbox的name = rememberMe
 		formAuthenticationFilter.setRememberMeParam("rememberMe");
 		return formAuthenticationFilter;
+	}
+
+	/**
+	 * shiro缓存管理器;
+	 * 需要添加到securityManager中
+	 * 
+	 * @return
+	 */
+	@Bean
+	public RedisCacheManager cacheManager() {
+		RedisCacheManager redisCacheManager = new RedisCacheManager();
+		redisCacheManager.setRedisManager(redisManager());
+		// redis中针对不同用户缓存
+		redisCacheManager.setPrincipalIdFieldName("username");
+		// 用户权限信息缓存时间
+		redisCacheManager.setExpire(200000);
+		return redisCacheManager;
+	}
+
+	@Bean
+	public RedisManager redisManager() {
+		RedisManager redisManager = new RedisManager();
+		redisManager.setHost("47.117.143.194");
+		redisManager.setPort(6379);
+		return redisManager;
+	}
+
+	/**
+	 * SessionDAO的作用是为Session提供CRUD并进行持久化的一个shiro组件
+	 * MemorySessionDAO 直接在内存中进行会话维护
+	 * EnterpriseCacheSessionDAO 提供了缓存功能的会话维护，默认情况下使用MapCache实现，内部使用ConcurrentHashMap保存缓存的会话。
+	 * 
+	 * @return
+	 */
+	@Bean
+	public SessionDAO sessionDAO() {
+		RedisSessionDAO redisSessionDAO = new RedisSessionDAO();
+		redisSessionDAO.setRedisManager(redisManager());
+		// session在redis中的保存时间,最好大于session会话超时时间
+		redisSessionDAO.setExpire(12000);
+		return redisSessionDAO;
+	}
+
+	/**
+	 * 配置保存sessionId的cookie
+	 * 注意：这里的cookie 不是上面的记住我 cookie 记住我需要一个cookie session管理 也需要自己的cookie
+	 * 默认为: JSESSIONID 问题: 与SERVLET容器名冲突,重新定义为sid
+	 * 
+	 * @return
+	 */
+	@Bean("sessionIdCookie")
+	public SimpleCookie sessionIdCookie() {
+		// 这个参数是cookie的名称
+		SimpleCookie simpleCookie = new SimpleCookie("sid");
+		// setcookie的httponly属性如果设为true的话，会增加对xss防护的安全系数。它有以下特点：
+
+		// setcookie()的第七个参数
+		// 设为true后，只能通过http访问，javascript无法访问
+		// 防止xss读取cookie
+		simpleCookie.setHttpOnly(true);
+		simpleCookie.setPath("/");
+		// maxAge=-1表示浏览器关闭时失效此Cookie
+		simpleCookie.setMaxAge(-1);
+		return simpleCookie;
+	}
+
+	/**
+	 * 配置会话管理器，设定会话超时及保存
+	 * 
+	 * @return
+	 */
+	@Bean("sessionManager")
+	public SessionManager sessionManager() {
+		DefaultWebSessionManager sessionManager = new DefaultWebSessionManager();
+		Collection<SessionListener> listeners = new ArrayList<SessionListener>();
+		// 配置监听
+		listeners.add(sessionListener());
+		sessionManager.setSessionListeners(listeners);
+		sessionManager.setSessionIdCookie(sessionIdCookie());
+		sessionManager.setSessionDAO(sessionDAO());
+		sessionManager.setCacheManager(cacheManager());
+
+		// 全局会话超时时间（单位毫秒），默认30分钟 暂时设置为10秒钟 用来测试
+		sessionManager.setGlobalSessionTimeout(1800000);
+		// 是否开启删除无效的session对象 默认为true
+		sessionManager.setDeleteInvalidSessions(true);
+		// 是否开启定时调度器进行检测过期session 默认为true
+		sessionManager.setSessionValidationSchedulerEnabled(true);
+		// 设置session失效的扫描时间, 清理用户直接关闭浏览器造成的孤立会话 默认为 1个小时
+		// 设置该属性 就不需要设置 ExecutorServiceSessionValidationScheduler 底层也是默认自动调用ExecutorServiceSessionValidationScheduler
+		// 暂时设置为 5秒 用来测试
+		sessionManager.setSessionValidationInterval(3600000);
+		// 取消url 后面的 JSESSIONID
+		sessionManager.setSessionIdUrlRewritingEnabled(false);
+		return sessionManager;
+
+	}
+
+	/**
+	 * 配置session监听
+	 * 
+	 * @return
+	 */
+	@Bean("sessionListener")
+	public ShiroSessionListener sessionListener() {
+		ShiroSessionListener sessionListener = new ShiroSessionListener();
+		return sessionListener;
 	}
 
 }
--- src/main/java/com/toolbox/util/CustomRealm.java
+++ src/main/java/com/toolbox/util/CustomRealm.java
@@ -1,11 +1,5 @@
 package com.toolbox.util;
 
-import com.toolbox.domain.User;
-import com.toolbox.mapper.UserMapper;
-import com.toolbox.service.LoginService;
-import com.toolbox.vo.PermissionsVO;
-import com.toolbox.vo.RoleVO;
-import com.toolbox.vo.UserVO;
 import org.apache.shiro.SecurityUtils;
 import org.apache.shiro.authc.*;
 import org.apache.shiro.authz.AuthorizationInfo;
@@ -15,69 +9,124 @@ import org.apache.shiro.subject.PrincipalCollection;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.util.StringUtils;
 
+import com.toolbox.domain.User;
+import com.toolbox.mapper.UserMapper;
+import com.toolbox.service.LoginService;
+import com.toolbox.vo.PermissionsVO;
+import com.toolbox.vo.RoleVO;
+import com.toolbox.vo.UserVO;
+
 public class CustomRealm extends AuthorizingRealm {
 
-    @Autowired
-    private LoginService loginService;
-    @Autowired
-    private UserMapper userMapper;
+	@Autowired
+	private LoginService loginService;
 
-    /**
-     * @MethodName doGetAuthorizationInfo
-     * @Description 权限配置类
-     * @Param [principalCollection]
-     * @Return AuthorizationInfo
-     */
-    @Override
-    protected AuthorizationInfo doGetAuthorizationInfo(PrincipalCollection principalCollection) {
-        //获取登录用户名
-        User user = (User) SecurityUtils.getSubject().getPrincipal();
-        // String name = (String) principalCollection.getPrimaryPrincipal().getClass().getName();
-        //查询用户名称
-        UserVO userVO = loginService.getUserByName(user.getUserName());
-        //添加角色和权限
-        SimpleAuthorizationInfo simpleAuthorizationInfo = new SimpleAuthorizationInfo();
-        for (RoleVO role : userVO.getRoleVOList()) {
-            //添加角色
-            simpleAuthorizationInfo.addRole(role.getRoleName());
-            //添加权限
-            for (PermissionsVO permissions : role.getPermissionsVOLis()) {
-                simpleAuthorizationInfo.addStringPermission(permissions.getPermissionsNam());
-            }
-        }
-        return simpleAuthorizationInfo;
-    }
+	@Autowired
+	private UserMapper userMapper;
 
-    /**
-     * @MethodName doGetAuthenticationInfo
-     * @Description 认证配置类
-     * @Param [authenticationToken]
-     * @Return AuthenticationInfo
-     */
-    @Override
-    protected AuthenticationInfo doGetAuthenticationInfo(AuthenticationToken authenticationToken) throws AuthenticationException {
-        if (StringUtils.isEmpty(authenticationToken.getPrincipal())) {
-            return null;
-        }
+	/**
+	 * @MethodName doGetAuthorizationInfo
+	 * @Description 权限配置类
+	 * @Param [principalCollection]
+	 * @Return AuthorizationInfo
+	 */
+	@Override
+	protected AuthorizationInfo doGetAuthorizationInfo(PrincipalCollection principalCollection) {
+		// 获取登录用户名
+		User user = (User) SecurityUtils.getSubject().getPrincipal();
+		// String name = (String) principalCollection.getPrimaryPrincipal().getClass().getName();
+		// 查询用户名称
+		UserVO userVO = loginService.getUserByName(user.getUserName());
+		// 添加角色和权限
+		SimpleAuthorizationInfo simpleAuthorizationInfo = new SimpleAuthorizationInfo();
+		for (RoleVO role : userVO.getRoleVOList()) {
+			// 添加角色
+			simpleAuthorizationInfo.addRole(role.getRoleName());
+			// 添加权限
+			for (PermissionsVO permissions : role.getPermissionsVOLis()) {
+				simpleAuthorizationInfo.addStringPermission(permissions.getPermissionsNam());
+			}
+		}
+		return simpleAuthorizationInfo;
+	}
 
-        //获取登录用户名
-        UsernamePasswordToken usernamePasswordToken = (UsernamePasswordToken) authenticationToken;
-        String username = usernamePasswordToken.getUsername();
-        String password = new String(usernamePasswordToken.getPassword());
+	/**
+	 * @MethodName doGetAuthenticationInfo
+	 * @Description 认证配置类
+	 * @Param [authenticationToken]
+	 * @Return AuthenticationInfo
+	 */
+	@Override
+	protected AuthenticationInfo doGetAuthenticationInfo(AuthenticationToken authenticationToken) throws AuthenticationException {
+		if (StringUtils.isEmpty(authenticationToken.getPrincipal())) {
+			return null;
+		}
 
-        //查询用户名称
-        User user = userMapper.queryByName(username);
-        if (user == null) {
-            throw new UnknownAccountException("用户名或密码错误！");
-        } else if (!password.equals(user.getPassword())) {
-            throw new UnknownAccountException("用户名或密码错误！");
+		// 获取登录用户名
+		UsernamePasswordToken usernamePasswordToken = (UsernamePasswordToken) authenticationToken;
+		String username = usernamePasswordToken.getUsername();
+		String password = new String(usernamePasswordToken.getPassword());
 
-        }
+		// 查询用户名称
+		User user = userMapper.queryByName(username);
+		if (user == null) {
+			throw new UnknownAccountException("用户名或密码错误！");
+		} else if (!password.equals(user.getPassword())) {
+			throw new UnknownAccountException("用户名或密码错误！");
 
-        //调用 CredentialsMatcher 校验 还需要创建一个类 继承CredentialsMatcher  如果在上面校验了,这个就不需要了
-        //配置自定义权限登录器 参考博客：
+		}
 
-        SimpleAuthenticationInfo info = new SimpleAuthenticationInfo(user, user.getPassword(), getName());
-        return info;
-    }
+		// 调用 CredentialsMatcher 校验 还需要创建一个类 继承CredentialsMatcher 如果在上面校验了,这个就不需要了
+		// 配置自定义权限登录器 参考博客：
+
+		SimpleAuthenticationInfo info = new SimpleAuthenticationInfo(user, user.getPassword(), getName());
+		return info;
+	}
+
+	/**
+	 * 重写方法,清除当前用户的的 授权缓存
+	 * 
+	 * @param principals
+	 */
+	@Override
+	public void clearCachedAuthorizationInfo(PrincipalCollection principals) {
+		super.clearCachedAuthorizationInfo(principals);
+	}
+
+	/**
+	 * 重写方法，清除当前用户的 认证缓存
+	 * 
+	 * @param principals
+	 */
+	@Override
+	public void clearCachedAuthenticationInfo(PrincipalCollection principals) {
+		super.clearCachedAuthenticationInfo(principals);
+	}
+
+	@Override
+	public void clearCache(PrincipalCollection principals) {
+		super.clearCache(principals);
+	}
+
+	/**
+	 * 自定义方法：清除所有 授权缓存
+	 */
+	public void clearAllCachedAuthorizationInfo() {
+		getAuthorizationCache().clear();
+	}
+
+	/**
+	 * 自定义方法：清除所有 认证缓存
+	 */
+	public void clearAllCachedAuthenticationInfo() {
+		getAuthenticationCache().clear();
+	}
+
+	/**
+	 * 自定义方法：清除所有的 认证缓存 和 授权缓存
+	 */
+	public void clearAllCache() {
+		clearAllCachedAuthenticationInfo();
+		clearAllCachedAuthorizationInfo();
+	}
 }
